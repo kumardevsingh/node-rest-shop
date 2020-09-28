6a33158ee1f147710e8e299d1eff6d69cae6bfb6
kumardevsingh <kumardevsingh@gmail.com>
2020-09-29 00:16:13 +0530

    refine the responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,12 +6,27 @@ const mongoose = require('mongoose');
 const Product = require('../modals/product');
 
 router.get('/', (req, res, next) => {
-    Product.find().
-        exec()
+    Product.find()
+        .select("_id name price")
+        .exec()
         .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {/* you can change the response here */
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+
+                })
+            }
             if (docs.length >= 0) {
-                console.log(docs);
-                res.status(200).json(docs)
+                res.status(200).json(response)
             } else {
                 res.status(404).json({
                     message: "No data found"
@@ -41,8 +56,16 @@ router.post('/', (req, res, next) => {
     });
     product.save().then((result) => {
         res.status(201).json({
-            message: 'a new product created',
-            createdProduct: product
+            message: ' Created product successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
         })
         console.log(result)
 
@@ -59,11 +82,17 @@ router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     console.log(id)
     Product.findById(id)
-
+        .select("_id name price")
         .then(doc => {
             if (doc) {
                 console.log(doc);
-                res.status(200).json(doc)
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + doc._id
+                    }
+                })
             } else {
                 res.status(404).json({
                     message: "Id not found"
@@ -89,9 +118,16 @@ router.patch('/:productId', (req, res, next) => {
     }
     Product.update({ _id: id }, { $set: updateOps }).exec()
         .then(result => {
-            console.log("in patch function:- ", result);
+
             if (result.nModified) {
-                res.status(200).json({ message: `id no: ${id} updated successfully!`, result })
+                res.status(200).json({
+                    product: result,
+                    message: `id no: ${id} updated successfully!`,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + id
+                    }
+                })
             } else {
                 res.status(404).json({ message: `id no: ${id} could not updated` })
             }
